7b75481ff35bd06429b38552e0eea03af66874f3
tsmerge <51195066+tsmerge@users.noreply.github.com>
2019-10-22 14:33:39 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
 let h1 = document.createElement('h1');
 h1.innerText = 'From Liao';
-document.body.prepend(h1);
+
+let div1 = document.createElement('div');
+div1.append(h1);
+
+document.body.prepend(div1);
