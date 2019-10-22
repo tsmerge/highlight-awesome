3e33b34b28a29876fe2626b4edb3360924ff5141
tsmerge <51195066+tsmerge@users.noreply.github.com>
2019-10-22 15:02:54 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 let h1 = document.createElement('h1');
-h1.innerText = 'I hope that Highlight Awesome will bring you efficiency and happiness :)';
+h1.innerText = 'Bring you efficiency :)';
 
 let div1 = document.createElement('div');
 div1.append(h1);
