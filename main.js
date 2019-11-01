4f2e4b11d7bd5c439b1da42d38a0054d77c55048
tsmerge <51195066+tsmerge@users.noreply.github.com>
2019-11-01 16:36:13 +0800

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ let h3 = document.createElement('h3');
 h3.innerHTML = "It brings you efficiency, really!<br>Believe me, try it for a month, you will love it, you can't live without it.";
 
 let div1 = document.createElement('div');
-div1.classList.add('content');
+div1.classList.add('body');
 div1.append(h3);
 
 // Append After, Via https://stackoverflow.com/a/32135318
